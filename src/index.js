cb353cbd4844792fa8d1074ed1d2cecc2cf05a66
akezeth <116703083+akezeth@users.noreply.github.com>
2023-05-10 10:41:01 +0000

    Set 'tasks' array

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const tasks = [
+  {
+    description: 'wash the dishes';
+    completed: false;
+    index: 0;
+  },
+  {
+    description: 'complete To Do list project';
+    completed: false;
+    index: 1;
+  },
+];
