354292d046fa3187502816082386da34b3e6d929
akezeth <116703083+akezeth@users.noreply.github.com>
2023-05-10 14:08:54 +0000

    Add displayTasks function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-const tasks = [
-  {
-    description: 'wash the dishes';
-    completed: false;
-    index: 0;
-  },
-  {
-    description: 'complete To Do list project';
-    completed: false;
-    index: 1;
-  },
-];
+import "./style.css";
+import tasks from "./modules/tasks";
+
+const displayTasks = () => {
+  tasks.forEach((task) => {
+    const taskContent = `
+      <li class="taskContent flex list">
+        <div class="taskInfo flex">
+          <input type="checkbox" class="completed-btn" ${
+            task.completed ? "checked" : ""
+          }>
+          <p>${task.description}</p>
+        </div>
+        <i class="fa-solid fa-ellipsis-vertical task-icon cross"></i>
+      </li>
+    `;
+    document.querySelector(".tasks").innerHTML += taskContent;
+  });
+};
+
+displayTasks();
