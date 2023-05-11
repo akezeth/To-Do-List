2afae9e42bd92b638b6fb46c06f46e7116f12af4
akezeth <116703083+akezeth@users.noreply.github.com>
2023-05-11 21:29:52 +0000

    Add editTask function

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
-import './style.css';
-import { saveTasks } from './modules/localStorage.js';
-import Tasks from './modules/Tasks.js';
-import newTask from './modules/newTask.js';
-import clearCompletedTasks from './modules/clearCompletedTasks.js';
+import "./style.css";
+import { saveTasks } from "./modules/localStorage.js";
+import Tasks from "./modules/Tasks.js";
+import newTask from "./modules/newTask.js";
+import clearCompletedTasks from "./modules/clearCompletedTasks.js";
+import editTask from "./modules/editTask.js";
 
-const addTaskBtn = document.querySelector('#addTaskBtn');
-const refreshBtn = document.querySelector('#refreshBtn');
+const addTaskBtn = document.querySelector("#addTaskBtn");
+const refreshBtn = document.querySelector("#refreshBtn");
 const tasks = new Tasks();
 
 tasks.display();
 // Add new task
-const newTaskForm = document.querySelector('[data-new-task-form]');
-newTaskForm.addEventListener('submit', (e) => {
+const newTaskForm = document.querySelector("[data-new-task-form]");
+newTaskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   newTask(tasks);
 });
-addTaskBtn.addEventListener('click', () => {
+addTaskBtn.addEventListener("click", () => {
   newTask(tasks);
 });
 // EventListener to check completed tasks
-const tasksContainer = document.querySelector('.tasks');
-tasksContainer.addEventListener('click', (e) => {
-  if (e.target.tagName.toLowerCase() === 'input') {
+const tasksContainer = document.querySelector(".tasks");
+tasksContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "input") {
     const selectedTask = tasks.taskArray.find(
-      (task) => task.index === parseInt(e.target.id, 10),
+      (task) => task.index === parseInt(e.target.id, 10)
     );
     selectedTask.complete = e.target.checked;
     saveTasks(tasks.taskArray);
   }
 });
 // clear all completed tasks
-const clearBtn = document.querySelector('[data-clear-completed-task]');
-clearBtn.addEventListener('click', () => {
+const clearBtn = document.querySelector("[data-clear-completed-task]");
+clearBtn.addEventListener("click", () => {
   clearCompletedTasks(tasks);
 });
-refreshBtn.addEventListener('click', () => {
+refreshBtn.addEventListener("click", () => {
   clearCompletedTasks(tasks);
 });
+
+editTask(tasks);
