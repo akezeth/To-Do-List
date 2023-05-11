33cffc70ee104d5b1d4febad9edc2c426eff754c
akezeth <116703083+akezeth@users.noreply.github.com>
2023-05-11 21:29:02 +0000

    Add editTask.js

---
+++ src/modules/editTask.js
@@ -0,0 +1,16 @@
+const editTask = (tasks) => {
+  for (let i = 0; i < tasks.taskArray.length; i += 1) {
+    const taskName = document.querySelectorAll(".task-name");
+    taskName[i].addEventListener("keydown", (e) => {
+      if (e.keyCode === 13) {
+        console.log("Aklilu Mekonnen");
+        e.preventDefault();
+      }
+    });
+    taskName[i].addEventListener("input", () => {
+      tasks.editTask(i, taskName[i].innerHTML);
+    });
+  }
+};
+
+export default editTask;
